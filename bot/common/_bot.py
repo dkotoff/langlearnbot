a2cba003a1b45e83dbec683941c97a731cf39c39
Daniil Kotov <100377787+dkotoff@users.noreply.github.com>
2024-03-18 11:06:36 +0300

    Update _bot.py

--- bot/common/_bot.py
+++ bot/common/_bot.py
@@ -6,10 +6,13 @@ from vkbottle.http import AiohttpClient
 from ._config import settings
 import aiohttp
 
+// подключение логирования
 logger.remove()
 logger.add(sys.stderr, level="INFO")
-
+// инициализация http клиента для подключения к вк
 http_client = AiohttpClient(connector=aiohttp.TCPConnector(verify_ssl=False))
 api = API(http_client=http_client, token=settings.TOKEN)
+
+// инициализация объекта бота
 bot = Bot(api=api)
 
