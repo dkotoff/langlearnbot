b762ed845de0eb281024d899dfd577607de9040e
Daniil Kotov <100377787+dkotoff@users.noreply.github.com>
2024-03-18 11:09:36 +0300

    Update _database.py

--- bot/common/_database.py
+++ bot/common/_database.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from ._config import settings
 
+#подключение к базе данных 
 engine = create_engine(
     url = settings.DATABASE_URI
 )
 
+#создания сессии для взаимодействия с базой данных 
 session = Session(bind=engine)
 
 
